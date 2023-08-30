b997907f84fa6c09c88d4c44f2d937199025d94e
Predrag Tasevski <stepcellwolf@gmail.com>
2023-08-30 22:31:13 +0200

    change the eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,8 @@ module.exports = {
   },
   plugins: ['react', '@typescript-eslint'],
   rules: {
-    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
   },
   settings: {
     react: {
