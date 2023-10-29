1c9dd9e6d7e69a5a6eadb4d94fabfc0c391a84ff
Yaroslav Bondarets <yar.bondarets@gmail.com>
2023-10-29 17:41:01 +0200

    No members limitation

--- pages/api/teams/[slug]/invitations.ts
+++ pages/api/teams/[slug]/invitations.ts
@@ -64,17 +64,17 @@ const handlePOST = async (req: NextApiRequest, res: NextApiResponse) => {
   const { slug } = req.query as { slug: string };
 
   //TODO: remove limits for community pack
-  if (role !== Role.MEMBER) {
-    const members = await getTeamMembers(slug);
+  // if (role !== Role.MEMBER) {
+  //   const members = await getTeamMembers(slug);
 
-    if (members.length >= 2) {
-      return res.status(400).json({
-        error: {
-          message: 'You have reached the maximum number of members per team.',
-        },
-      });
-    }
-  }
+  //   if (members.length >= 2) {
+  //     return res.status(400).json({
+  //       error: {
+  //         message: 'You have reached the maximum number of members per team.',
+  //       },
+  //     });
+  //   }
+  // }
 
   const invitationExists = await prisma.invitation.findFirst({
     where: {
