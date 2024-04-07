43ad89fcd8e19a9774110a5b9404effdbec52544
Predrag Tasevski <stepcellwolf@gmail.com>
2024-04-07 22:56:59 +0200

    Tracking without custom options

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,22 @@
 import { Head, Html, Main, NextScript } from 'next/document';
 
-export default function Document() {
+export default function Document(props) {
   return (
     <Html lang="en" className="h-full" data-theme="boxyhq">
-      <Head />
+      <Head>
+        <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,t,u,n,a,m){w['MauticTrackingObject']=n;
+                w[n]=w[n]||function(){(w[n].q=w[n].q||[]).push(arguments)},a=d.createElement(t),
+                m=d.getElementsByTagName(t)[0];a.async=1;a.src=u;m.parentNode.insertBefore(a,m)
+                })(window,document,'script','https://crm.unicis.tech/mtc.js','mt');
+
+                mt('send', 'pageview');
+              `
+            }}
+          />
+      </Head>
       <body className="h-full">
         <Main />
         <NextScript />
