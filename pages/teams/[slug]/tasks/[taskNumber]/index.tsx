a683a929fab28df8ac29e49021daecaaf1d1af30
Abdulsamad Opeyemi Abdulganiyu <agastronics@gmail.com>
2024-05-31 19:55:22 +0100

    Modified: Breadcrumb

--- pages/teams/[slug]/tasks/[taskNumber]/index.tsx
+++ pages/teams/[slug]/tasks/[taskNumber]/index.tsx
@@ -58,7 +58,7 @@ const TaskById = ({
 
   return (
     <>
-      <Breadcrumb taskTitle='Team' backTo={`/teams/${slug}/tasks`} teamName={ slug as string } taskNumber={taskNumber as string} />
+      <Breadcrumb taskTitle={task.title} backTo={`/teams/${slug}/tasks`} teamName={ slug as string } taskNumber={taskNumber as string} />
       <h3 className="text-2xl font-bold">{task.title}</h3>
       <TaskTab activeTab={activeTab} setActiveTab={setActiveTab} />
       {activeTab === 'Overview' && (
