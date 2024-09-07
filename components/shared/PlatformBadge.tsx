54eaaaf2769560b677715e008eec290e7c67d775
Abdulsamad Opeyemi Abdulganiyu <agastronics@gmail.com>
2024-09-07 14:33:09 +0100

    Modified | Add

--- components/shared/PlatformBadge.tsx
+++ components/shared/PlatformBadge.tsx
@@ -1,21 +1,52 @@
 import { Badge as BaseBadge } from 'react-daisyui';
+import LinuxIcon from '@/public/icons/linux.svg';
+import WindowsIcon from '@/public/icons/windows-7.svg';
+import MacIcon from '@/public/icons/os-apple.svg';
+import FreeBSDIcon from '@/public/icons/freebsd-outlined.svg';
+import PosixIcon from '@/public/icons/pc.svg';
+import AllPlatformsIcon from '@/public/icons/pc.svg';
+import Image from 'next/image';
+
 
 const colors = {
   linux: 'ghost',
-  window: 'secondary',
+  windows: 'secondary',
   darwin: 'primary',
   freebsd: 'info',
   posix: 'success',
   all: 'accent',
 };
 
+const platformIcons = {
+  linux: LinuxIcon,
+  windows: WindowsIcon,
+  darwin: MacIcon,
+  freebsd: FreeBSDIcon,
+  posix: PosixIcon,
+  all: AllPlatformsIcon,
+};
+
 const PlatformBadge = ({ label, value }: { label: string; value: string }) => {
+  const Icon = platformIcons[value];
+
   return (
     <>
       <BaseBadge
-        className={`rounded text-xs py-2 text-white whitespace-nowrap`}
-        color={colors[value]}
+        className={`rounded text-xs py-2 text-white whitespace-nowrap flex items-center`}
+        // color={colors[value]}
       >
+        {Icon && (
+          <div style={{ filter: 'invert(100%)' }}>
+            <Image
+              src={Icon}
+              alt={`${value} icon`}
+              width={16}
+              layout='fixed'
+              height={16}
+              className="mr-2"
+            />
+          </div>
+        )}
         {label}
       </BaseBadge>
     </>
