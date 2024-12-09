cbf812230242e55ebac77228453700a1f39d9c42
Yaroslav Bondarets <yar.bondarets@gmail.com>
2024-12-09 15:06:20 +0200

    #35 Risk managament table

--- components/interfaces/RiskManagement/RisksTable.tsx
+++ components/interfaces/RiskManagement/RisksTable.tsx
@@ -1,20 +1,46 @@
 import React from 'react';
 import Link from 'next/link';
-import { SimpleTag as Tag } from '@atlaskit/tag';
-import statuses from '@/components/defaultLanding/data/statuses.json';
 import type { TaskWithRmRisk } from 'types';
 import { Button } from 'react-daisyui';
 import { useTranslation } from 'next-i18next';
 import usePagination from 'hooks/usePagination';
 import { TailwindTableWrapper } from 'sharedStyles';
-import { StatusBadge } from '@/components/shared';
 import useCanAccess from 'hooks/useCanAccess';
-import Badge from '@/components/shared/Badge';
-// import { riskProbabilityPoints, riskSecurityPoints } from '@/components/defaultLanding/data/configs/pia';
 
-const toPercentage = (input: number): string => {
-    const percentage = (input / 25) * 100;
-    return `${percentage.toFixed(0)}%`;
+const verticalTextStyles =
+    "px-1.5 py-1.5 whitespace-nowrap align-middle text-center [writing-mode:vertical-rl] rotate-180";
+
+const calculateRiskRating = (probability: number, impact: number) => {
+    const result = (probability / 100) * (impact / 100);
+    return Math.floor(result * 100);
+}
+
+const calculateCurrentRiskRating = (rawRiskRating: number, targetRiskRating: number, treatmentStatus: number) => {
+    const decimalTreatmentStatus = treatmentStatus / 100 // 50% -> 0.5
+    const result = rawRiskRating - (decimalTreatmentStatus * (rawRiskRating - targetRiskRating))
+    return Math.floor(result)
+}
+
+const getGradientColor = (value: number): string => {
+    // Ensure value is between 0 and 100
+    const clampedValue = Math.max(0, Math.min(100, value));
+
+    // Interpolate between green (0, 255, 0) and red (255, 0, 0)
+    const red = Math.floor((clampedValue / 100) * 255);
+    const green = 255 - red;
+
+    return `rgb(${red}, ${green}, 0)`;
+};
+
+const getInitials = (name: string) => {
+    const words = name.trim().split(" ");
+
+    const initials = words
+        .filter(word => word.length > 0)
+        .map(word => word[0].toUpperCase())
+        .join("");
+
+    return initials;
 }
 
 const RisksTable = ({
@@ -50,20 +76,47 @@ const RisksTable = ({
                         <thead className="bg-base-200 dark:bg-gray-700 dark:text-gray-400">
                             <tr>
                                 <th scope="col" className="px-1.5 py-1.5">
-                                    {t('rpa')}
+                                    Risk ID
                                 </th>
-                                <th scope="col" className="px-1.5 py-1.5">
-                                    {t('status')}
+                                <th>
+                                    Risk
                                 </th>
-                                {/* <th scope="col" className="px-1.5 py-1.5">
-                                    Confidentiality and Integrity
+                                <th className={verticalTextStyles}>
+                                    Asset Owner
                                 </th>
-                                <th scope="col" className="px-1.5 py-1.5">
-                                    Availability
+                                <th>
+                                    Impact
                                 </th>
-                                <th scope="col" className="px-1.5 py-1.5">
-                                    Transparency, purpose limitation and data minimization
-                                </th> */}
+                                <th className={verticalTextStyles}>
+                                    Raw probabiltity
+                                </th>
+                                <th className={verticalTextStyles}>
+                                    Raw impact
+                                </th>
+                                <th className={verticalTextStyles}>
+                                    Raw risk rating
+                                </th>
+                                <th>
+                                    Treatment
+                                </th>
+                                <th className={verticalTextStyles}>
+                                    Treatment cost
+                                </th>
+                                <th className={verticalTextStyles}>
+                                    Treatment status
+                                </th>
+                                <th className={verticalTextStyles}>
+                                    Treated probability
+                                </th>
+                                <th className={verticalTextStyles}>
+                                    Treated impact
+                                </th>
+                                <th className={verticalTextStyles}>
+                                    Targer risk rating
+                                </th>
+                                <th className={verticalTextStyles}>
+                                    Current risk rating
+                                </th>
                                 {canAccess('task', ['update']) && (
                                     <th scope="col" className="px-1.5 py-1.5">
                                         {t('actions')}
@@ -72,72 +125,91 @@ const RisksTable = ({
                             </tr>
                         </thead>
                         <tbody>
-                            {pageData.map((task, index) => (
-                                <tr
-                                    key={index}
-                                    className="border-b bg-white hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600"
-                                >
-                                    <td className="px-1.5 py-1.5">
-                                        <Link href={`/teams/${slug}/tasks/${task.taskNumber}`}>
-                                            <div className="flex items-center justify-start space-x-2">
-                                                <span className="underline">{task.title}</span>
-                                            </div>
-                                        </Link>
-                                    </td>
-                                    <td className="px-1.5 py-1.5">
-                                        <StatusBadge
-                                            label={
-                                                statuses.find(({ value }) => value === task.status)
-                                                    ?.label as string
-                                            }
-                                            value={task.status}
-                                        />
-                                    </td>
-                                    {/* <td className="px-1.5 py-1.5">
-                                        {toPercentage(
-                                            riskProbabilityPoints[task.properties.pia_risk[1].confidentialityRiskProbability] *
-                                            riskSecurityPoints[task.properties.pia_risk[1].confidentialityRiskSecurity]
-                                        )}
-                                    </td>
-                                    <td className="px-1.5 py-1.5">
-                                        {toPercentage(
-                                            riskProbabilityPoints[task.properties.pia_risk[2].availabilityRiskProbability] *
-                                            riskSecurityPoints[task.properties.pia_risk[2].availabilityRiskSecurity]
-                                        )}
-                                    </td>
-                                    <td className="px-1.5 py-1.5">
-                                        {toPercentage(
-                                            riskProbabilityPoints[task.properties.pia_risk[3].transparencyRiskProbability] *
-                                            riskSecurityPoints[task.properties.pia_risk[3].transparencyRiskSecurity]
-                                        )}
-                                    </td> */}
-                                    {canAccess('task', ['update']) && (
-                                        <td className="px-1.5 py-1.5">
-                                            <div className="btn-group">
-                                                <Button
-                                                    size="sm"
-                                                    variant="outline"
-                                                    onClick={() => {
-                                                        editHandler(task);
-                                                    }}
-                                                >
-                                                    {t('edit-task')}
-                                                </Button>
-
-                                                <Button
-                                                    size="sm"
-                                                    variant="outline"
-                                                    onClick={() => {
-                                                        deleteHandler(task);
-                                                    }}
-                                                >
-                                                    {t('delete')}
-                                                </Button>
-                                            </div>
+                            {pageData.map((task, index) => {
+                                const risk = task.properties.rm_risk
+                                const rawRiskRating = calculateRiskRating(risk[0].RawProbability, risk[0].RawImpact)
+                                const targetRiskRating = calculateRiskRating(risk[1].TreatedProbability, risk[1].TreatedImpact)
+                                const currentRiskRating = calculateCurrentRiskRating(rawRiskRating, targetRiskRating, risk[1].TreatmentStatus)
+                                
+                                return (
+                                    <tr
+                                        key={index}
+                                        className="border-b bg-white hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600"
+                                    >
+                                        <td>
+                                            <Link href={`/teams/${slug}/tasks/${task.taskNumber}`}>
+                                                <div className="flex items-center justify-start space-x-2">
+                                                    <span className="underline">{task.title}</span>
+                                                </div>
+                                            </Link>
                                         </td>
-                                    )}
-                                </tr>
-                            ))}
+                                        <td>
+                                            <span>{risk[0].Risk}</span>
+                                        </td>
+                                        <td>
+                                            <span>{getInitials(risk[0].AssetOwner.label)}</span>
+                                        </td>
+                                        <td>
+                                            <span>{risk[0].Impact}</span>
+                                        </td>
+                                        <td>
+                                            <span>{risk[0].RawProbability}%</span>
+                                        </td>
+                                        <td>
+                                            <span>{risk[0].RawImpact}%</span>
+                                        </td>
+                                        <td style={{backgroundColor: getGradientColor(rawRiskRating)}}>
+                                            {rawRiskRating}%
+                                        </td>
+                                        <td>
+                                            {risk[1].RiskTreatment}
+                                        </td>
+                                        <td>
+                                            {risk[1].TreatmentCost}
+                                        </td>
+                                        <td>
+                                            {risk[1].TreatmentStatus}%
+                                        </td>
+                                        <td>
+                                            {risk[1].TreatedProbability}%
+                                        </td>
+                                        <td>
+                                            {risk[1].TreatedImpact}%
+                                        </td>
+                                        <td style={{ backgroundColor: getGradientColor(targetRiskRating)}}>
+                                            {targetRiskRating}%
+                                        </td>
+                                        <td style={{ backgroundColor: getGradientColor(currentRiskRating)}}>
+                                            {currentRiskRating}%
+                                        </td>
+                                        {canAccess('task', ['update']) && (
+                                            <td className="px-1.5 py-1.5">
+                                                <div className="btn-group">
+                                                    <Button
+                                                        size="sm"
+                                                        variant="outline"
+                                                        onClick={() => {
+                                                            editHandler(task);
+                                                        }}
+                                                    >
+                                                        {t('edit-task')}
+                                                    </Button>
+
+                                                    <Button
+                                                        size="sm"
+                                                        variant="outline"
+                                                        onClick={() => {
+                                                            deleteHandler(task);
+                                                        }}
+                                                    >
+                                                        {t('delete')}
+                                                    </Button>
+                                                </div>
+                                            </td>
+                                        )}
+                                    </tr>
+                                )
+                            })}
                         </tbody>
                     </table>
                 </div>
