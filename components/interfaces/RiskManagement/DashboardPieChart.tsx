4be72a8b28d49607a7e9b97c149952480ced7ec5
Yaroslav Bondarets <yar.bondarets@gmail.com>
2025-01-17 00:44:35 +0200

    #35 RM dashboard pie chart colors changed

--- components/interfaces/RiskManagement/DashboardPieChart.tsx
+++ components/interfaces/RiskManagement/DashboardPieChart.tsx
@@ -4,26 +4,34 @@ import { Pie } from 'react-chartjs-2';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
+// const barColors = [
+//     'rgba(1, 107, 1)',
+//     'rgba(72, 135, 0)',
+//     'rgba(255, 176, 5)',
+//     'rgba(255, 111, 3)',
+//     'rgb(254, 1, 0)',
+//   ];
+
 const barColors = [
-    'rgba(1, 107, 1)',
-    'rgba(72, 135, 0)',
-    'rgba(255, 176, 5)',
-    'rgba(255, 111, 3)',
-    'rgb(254, 1, 0)',
-  ];
+  'rgba(47, 143, 0, 1)',
+  'rgba(106, 217, 0, 1)',
+  'rgba(255, 190, 0, 1)',
+  'rgba(255, 111, 3)',
+  'rgba(255, 0, 0, 1)',
+];
 
 const DashboardChart = ({
-    datasets
+  datasets
 }: {
-    datasets: number[]
+  datasets: number[]
 }) => {
   const data = {
     labels: [
-        'Insignificant',
-        'Minor',
-        'Moderate',
-        'Major',
-        'Extreme',
+      'Insignificant',
+      'Minor',
+      'Moderate',
+      'Major',
+      'Extreme',
     ],
     datasets: [
       {
