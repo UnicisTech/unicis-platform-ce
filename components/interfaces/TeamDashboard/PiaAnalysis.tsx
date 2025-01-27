57a3915e5f9882fb65ba6e67b36e8ed1b64b722f
Yaroslav Bondarets <yar.bondarets@gmail.com>
2025-01-27 21:05:25 +0200

    #34 PIA Dasboard matixes flex-column for mobile view

--- components/interfaces/TeamDashboard/PiaAnalysis.tsx
+++ components/interfaces/TeamDashboard/PiaAnalysis.tsx
@@ -25,7 +25,7 @@ const PiaAnalysis = ({ tasks }: PiaAnalysisProps) => {
         <>
             <h4>{t("pia-overview")}</h4>
             <div className="w-full p-4 shadow-lg mx-3">
-                <div className="flex gap-6 overflow-x-scroll">
+                <div className="flex flex-col md:flex-row justify-center gap-6 overflow-x-scroll">
                     {riskSections.map(({ title, id, map }, index) => (
                         <div>
                         <div key={id} className="flex-1 text-center text-lg font-semibold">
