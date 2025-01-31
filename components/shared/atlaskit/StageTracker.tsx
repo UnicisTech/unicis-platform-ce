04760d84dac462555a521758067e071061cc9589
Yaroslav Bondarets <yar.bondarets@gmail.com>
2025-01-31 14:41:22 +0200

    #53 StageTracker dark theme color fixes

--- components/shared/atlaskit/StageTracker.tsx
+++ components/shared/atlaskit/StageTracker.tsx
@@ -1,6 +1,22 @@
 import { ProgressTracker, type Stages } from '@atlaskit/progress-tracker';
+import styled from 'styled-components';
+import useTheme from 'hooks/useTheme';
+import { Fragment } from 'react';
+
+const StageTrackerWrapper = styled.div`
+  div[style*="color: var(--ds-text"] a {
+    color: var(--ds-text-subtlest, #626F86) !important;
+  }
+  div[style*="color: var(--ds-text-subtlest"] {
+    color: white !important;
+  }
+`;
 
 const StageTracker = ({ currentStage, headers }: { currentStage: number, headers: string[] }) => {
+  const { theme } = useTheme();
+  
+  const Wrapper = theme === 'dark' ? StageTrackerWrapper : Fragment
+
   const items: Stages = Array(headers.length).fill(null).map((_, index) => ({
     id: `step-${index + 1}`,
     label: headers[index],
@@ -9,7 +25,11 @@ const StageTracker = ({ currentStage, headers }: { currentStage: number, headers
     href: '#',
   }));
 
-  return <ProgressTracker items={items} spacing="compact" />;
+  return (
+    <Wrapper>
+      <ProgressTracker items={items} spacing="compact" />
+    </Wrapper>
+  );
 };
 
 export default StageTracker;
