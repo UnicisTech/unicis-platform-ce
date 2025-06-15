ebf2af670b421efeacda88cea83771389afbf3de
Yaroslav Bondarets <yar.bondarets@gmail.com>
2025-06-15 19:07:00 +0300

    OP#264 Empty state component dark theme fix

--- components/shared/EmptyState.tsx
+++ components/shared/EmptyState.tsx
@@ -1,20 +1,20 @@
-import InformationCircleIcon from '@heroicons/react/24/outline/InformationCircleIcon';
-import React from 'react';
+import { InformationCircleIcon } from "@heroicons/react/24/outline";
+import React from "react";
 
 interface EmptyStateProps {
   title: string;
   description?: string;
 }
 
-const EmptyState = ({ title, description }: EmptyStateProps) => {
+const EmptyState: React.FC<EmptyStateProps> = ({ title, description }) => {
   return (
-    <div className="flex w-full flex-col items-center justify-center rounded lg:p-20 border gap-2 bg-white dark:bg-black h-80 border-slate-300 dark:border-white">
-      <InformationCircleIcon className="w-10 h-10" />
-      <h3 className="text-semibold text-emphasis text-center text-lg">
+    <div className="flex w-full flex-col items-center justify-center rounded-lg p-20 border bg-background border-border h-80 gap-2">
+      <InformationCircleIcon className="w-10 h-10 text-muted-foreground" />
+      <h3 className="text-lg font-semibold text-foreground text-center">
         {title}
       </h3>
       {description && (
-        <p className="text-default text-center font-light leading-6 text-sm">
+        <p className="text-sm font-light text-muted-foreground text-center leading-6">
           {description}
         </p>
       )}
