183f31cabb745b93874a59aaca1cabce934a92fd
Yaroslav Bondarets <yar.bondarets@gmail.com>
2025-10-10 00:49:17 +0300

    CSC Dashboard overflow-x fix

--- components/interfaces/CSC/StatusesTable.tsx
+++ components/interfaces/CSC/StatusesTable.tsx
@@ -81,7 +81,7 @@ const StatusesTable = ({
 
   return (
     <div className="[&_th]:whitespace-normal! [&_td]:whitespace-normal!">
-      <div className="overflow-x- mt-2">
+      <div className="overflow-x-auto mt-2">
         <table className="w-full min-w-full divide-y divide-border text-sm">
           <thead className="bg-muted">
             <tr>
