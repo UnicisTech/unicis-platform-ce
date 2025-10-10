fd68252ee8ad80794483c1fca739356dee73a855
Yaroslav Bondarets <yar.bondarets@gmail.com>
2025-10-10 12:35:35 +0300

    Team setting CSC frameworks mobile view fix

--- components/team/CSCSettings.tsx
+++ components/team/CSCSettings.tsx
@@ -31,12 +31,11 @@ const CSCSettings: React.FC<CSCSettingsProps> = ({ team }) => {
   const frameworkOptions = isoOptions.map((option) => {
     const isDisabled = !avaliableISO.includes(option.value);
     const message = isDisabled
-      ? ` - ${
-          subscriptionParams.ULTIMATE.avaliableISO.includes(option.value) &&
-          !subscriptionParams.PREMIUM.avaliableISO.includes(option.value)
-            ? t('csc-ultimate only')
-            : t('csc-premium-and-ultimate only')
-        }`
+      ? ` - ${subscriptionParams.ULTIMATE.avaliableISO.includes(option.value) &&
+        !subscriptionParams.PREMIUM.avaliableISO.includes(option.value)
+        ? t('csc-ultimate only')
+        : t('csc-premium-and-ultimate only')
+      }`
       : null;
 
     return {
@@ -87,7 +86,7 @@ const CSCSettings: React.FC<CSCSettingsProps> = ({ team }) => {
 
         <CardContent className="space-y-4">
           <p>{t('csc-choose-iso')}</p>
-          <div className="flex items-center space-x-3 w-1/2">
+          <div className="flex flex-col lg:flex-row gap-4">
             <div className="flex-1">
               <Label htmlFor="iso" className="sr-only">
                 {t('iso')}
@@ -103,13 +102,15 @@ const CSCSettings: React.FC<CSCSettingsProps> = ({ team }) => {
                 defaultValue={teamProperties.csc_iso}
               />
             </div>
-            <Button
-              type="submit"
-              disabled={!formik.isValid || !formik.dirty || formik.isSubmitting}
-            >
-              {formik.isSubmitting && <Loader2 className="animate-spin" />}
-              {t('choose')}
-            </Button>
+            <div className='items-center pt-0 flex justify-end'>
+              <Button
+                type="submit"
+                disabled={!formik.isValid || !formik.dirty || formik.isSubmitting}
+              >
+                {formik.isSubmitting && <Loader2 className="animate-spin" />}
+                {t('choose')}
+              </Button>
+            </div>
           </div>
         </CardContent>
       </Card>
