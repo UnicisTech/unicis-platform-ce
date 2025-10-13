a65f53506bf900c461c4743dd7de719d8907c984
Yaroslav Bondarets <yar.bondarets@gmail.com>
2025-10-13 21:34:44 +0300

    /shadcn/ui/button.tsx subtle variant added

--- components/shadcn/ui/button.tsx
+++ components/shadcn/ui/button.tsx
@@ -18,6 +18,7 @@ const buttonVariants = cva(
         secondary:
           'bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
+        subtle: 'text-secondary-foreground hover:bg-secondary/80',
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
