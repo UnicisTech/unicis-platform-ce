b522701e343b708f8fa1a442f6d481b69476b907
Yaroslav Bondarets <yar.bondarets@gmail.com>
2026-01-31 18:16:15 +0200

    shadcn/card title text size increased

--- components/shadcn/ui/card.tsx
+++ components/shadcn/ui/card.tsx
@@ -35,7 +35,10 @@ const CardTitle = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn('font-semibold leading-none tracking-tight', className)}
+    className={cn(
+      'text-lg font-semibold leading-none tracking-tight',
+      className
+    )}
     {...props}
   />
 ));
