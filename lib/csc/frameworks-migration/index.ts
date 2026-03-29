a0c7b3dd67a810796bb34670759ecf1a9e363788
Yaroslav Bondarets <yar.bondarets@gmail.com>
2026-03-30 01:39:06 +0300

    removed unnecessary csc framework migrations file

--- lib/csc/frameworks-migration/index.ts
+++ lib/csc/frameworks-migration/index.ts
@@ -7,9 +7,6 @@ import eu_nis2 from './eu_nis2';
 import gdpr from './gdpr';
 import cis_v_81 from './cis_v_81';
 import soc2V2 from './soc2-v2';
-import owasp_asvs_v5 from './owasp_asvs_v5';
-import pcidss_v401 from './pcidss_v401';
-import iso42001 from './iso42001';
 
 import { ISO } from 'types';
 
@@ -32,9 +29,9 @@ const frameworks = {
   cisv81: cis_v_81,
   soc2v2: soc2V2,
   c5_2020: c5_2020,
-  owasp_asvs_v5: owasp_asvs_v5,
-  pcidss_v401: pcidss_v401,
-  iso42001: iso42001,
+  // owasp_asvs_v5: owasp_asvs_v5,
+  // pcidss_v401: pcidss_v401,
+  // iso42001: iso42001,
 };
 
 export default frameworks as Record<ISO, CscFremaworkControl[]>;
